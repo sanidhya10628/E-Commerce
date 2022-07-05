5aa95cfd49e8be933efdbe863e3e08b3f7cba006
Sanidhya Mahajan <sanidhya10628@gmail.com>
2022-07-05 23:16:55 +0530

    Website Completed

--- context/StateContext.js
+++ context/StateContext.js
@@ -17,7 +17,7 @@ export const StateContext = ({ children }) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
         setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
-
+        setQty(1);
         if (checkProductInCart) {
 
             const updatedCartItems = cartItems.map((cartProduct) => {

ed8f1971332c945d62f957f059cf0fe2204a0234
Sanidhya Mahajan <sanidhya10628@gmail.com>
2022-12-05 15:37:09 +0530

    Initial Commit

---
+++ sanity_ecommerce/schemas/customer.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'customer',
+    title: 'Customer',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'email',
+            title: 'Email',
+            type: 'string'
+        }
+
+    ]
+}
